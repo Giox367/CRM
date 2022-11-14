ae0968cd17828126e11b3b3033170977f2dfb2b7
giox367 <111007060+Giox367@users.noreply.github.com>
2022-11-14 15:59:59 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterPage } from './pages/register/register.page';
-import { LoginPage } from './pages/login/login.page';
+import { RegisterPage } from './auth/register/register.page';
+import { LoginPage } from './auth/login/login.page';
+import { HomePage } from './pages/home/home.page';
+import { UsersPage } from './pages/users/users.page';
+import { ClientsPage } from './pages/clients/clients.page';
+import { InvoicesPage } from './pages/invoices/invoices.page';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   pathMatch: 'full',
-  //   redirectTo: 'login-page',
-  // },
   {
-    path: 'login-page',
-    component: LoginPage,
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomePage,
+  },
+  {
+    path: 'users',
+    component: UsersPage,
+  },
+  {
+    path: 'invoices',
+    component: InvoicesPage,
+  },
+  {
+    path: 'clients',
+    component: ClientsPage,
   },
   {
     path: 'users-details',
-    loadChildren: () =>
-      import('./pages/users-details/users-details.module').then(
-        (m) => m.UsersDetailsModule
-      ),
-  },
-  {
-    path: 'register',
-    component: RegisterPage,
+    loadChildren: () => import('./pages/users-details/users-details.module').then((m) => m.UsersDetailsModule),
   },
 ];
 
