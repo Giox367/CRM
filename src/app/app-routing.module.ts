8cf74c6603689514a09bfadaec570f277ab49788
giox367 <111007060+Giox367@users.noreply.github.com>
2022-11-14 11:03:27 +0100

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'users-details', loadChildren: () => import('./users-details/users-details.module').then(m => m.UsersDetailsModule) }];
+const routes: Routes = [
+  {
+    path: 'users-details',
+    loadChildren: () =>
+      import('./pages/users-details/users-details.module').then(
+        (m) => m.UsersDetailsModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
