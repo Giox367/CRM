cbc2da240a68be974bc8fd944d059f5fc6944d63
gigijeskai <luigindaco@gmail.com>
2022-11-14 17:46:02 +0100

    aggiunta card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomePage } from './pages/home/home.page';
 import { UsersPage } from './pages/users/users.page';
 import { ClientsPage } from './pages/clients/clients.page';
 import { InvoicesPage } from './pages/invoices/invoices.page';
+import { InvoicesDetailsPage } from './pages/invoices/invoices-details/invoices-details.page';
 
 const routes: Routes = [
   {
