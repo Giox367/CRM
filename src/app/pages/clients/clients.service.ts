049bab34e57c04f068fbeaf8edb7b0e6eb8bc180
dimitrilzk <dimitrilazuka95@gmail.com>
2022-11-15 18:04:50 +0100

    a

---
+++ src/app/pages/clients/clients.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientsService {
+
+  urlApiClients = "./db.json"
+
+
+
+  constructor() { }
+
+
+}
